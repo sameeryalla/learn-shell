acedb6e764ba0c40cf72839ad03cda34a2637418
sameeryalla <sameeryalla@gmail.com>
2023-04-16 21:52:57 +0530

    Frontend shell scrip file modification for roboshop config file

--- Frontend.sh
+++ Frontend.sh
@@ -5,5 +5,5 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
-mv roboshop.conf /etc/nginx/default.d/ 
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf 
 systemctl restart nginx
