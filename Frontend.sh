abb3a4117360d1ff3304842c72309cb23e7a1375
sameeryalla <sameeryalla@gmail.com>
2023-04-16 21:47:01 +0530

    Fronrend shell script file

--- Frontend.sh
+++ Frontend.sh
@@ -1,15 +1,9 @@
 yum install nginx -y 
-
 systemctl enable nginx 
 systemctl start nginx 
-
 rm -rf /usr/share/nginx/html/* 
-
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
-
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
-
 mv roboshop.conf /etc/nginx/default.d/ -y
-
-systemctl restart nginx 
+systemctl restart nginx
