102bb07a6e85f8f44bccc5868e427380ed25ab6a
sameeryalla <sameeryalla@gmail.com>
2023-04-16 22:01:43 +0530

    Frontend module config changes

--- Frontend.sh
+++ Frontend.sh
@@ -1,9 +1,8 @@
 yum install nginx -y 
-systemctl enable nginx 
-systemctl start nginx 
+systemctl enable nginx
 rm -rf /usr/share/nginx/html/* 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf 
+#cp roboshop.conf /etc/nginx/default.d/roboshop.conf 
 systemctl restart nginx
