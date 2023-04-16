b9ca99ad83338bc437128566565588d6c13a09e5
sameeryalla <sameeryalla@gmail.com>
2023-04-16 21:44:06 +0530

    Frontend shell script file

--- Frontend.sh
+++ Frontend.sh
@@ -10,6 +10,6 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 
-cp roboshop.conf /etc/nginx/default.d/
+mv roboshop.conf /etc/nginx/default.d/ -y
 
 systemctl restart nginx 
