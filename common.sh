71c2451cd6067b65a9fd751deaf6238b4b59028a
sameeryalla <sameeryalla@gmail.com>
2023-04-21 23:01:43 +0530

    Code dry process

--- common.sh
+++ common.sh
@@ -12,7 +12,8 @@ func_status_check()
   if [ $1 -eq 0 ]; then
       echo -e "\e[32m SUCCESS \e[0m"
     else
-      echo -e "\e[31m FAILED \e[0m"
+      echo -e "\e[31m FAILED, refer the log file /tmp/Roboshop.log for more info \e[0m"
+
       exit 1
   fi
 }
@@ -21,19 +22,18 @@ func_status_check()
 func_app_prereq()
 {
     func_print_head " add ${app_user} user "
-    useradd ${app_user}
+    useradd ${app_user} &>>/tmp/Roboshop.log
     func_status_check $?
     func_print_head " create application directory"
     rm -rf /app
-    mkdir /app
+    mkdir /app &>>/tmp/Roboshop.log
     func_status_check $?
     func_print_head " download ${component} source code"
-    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>/tmp/Roboshop.log
     func_status_check $?
     func_print_head " navigate to app folder and unzip $component source code"
     cd /app
-    unzip /tmp/${component}.zip
-    # shellcheck disable=SC1072
+    unzip /tmp/${component}.zip &>>/tmp/Roboshop.log
     func_status_check $?
 
 }
@@ -41,11 +41,10 @@ func_app_prereq()
 func_systemd_setup()
 {
   func_print_head " copy ${component} service file to /etc/systemd/system/${component}.service"
-  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
-  func_status_check $?
+  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service &>>/tmp/Roboshop.log
   func_status_check $?
   func_print_head " reload the ${component} service"
-  systemctl daemon-reload
+  systemctl daemon-reload &>>/tmp/Roboshop.log
   systemctl enable ${component}
   systemctl start ${component}
   func_status_check $?
@@ -55,20 +54,21 @@ func_schema_setup()
 {
   if [ "$schema_setup" == "mongo" ]; then
       func_print_head copy mongodb repo file to the path /etc/yum.repos.d/mongo.repo
-      cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+      cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo &>/tmp/Roboshop.log
+      func_status_check $?
       func_print_head Install mongodb shell
-      yum install mongodb-org-shell -y
+      yum install mongodb-org-shell -y &>>/tmp/Roboshop.log
       func_status_check $?
       func_print_head load mongodb schema
-      mongo --host mongodb.sameerdevops.online </app/schema/${component}.js
+      mongo --host mongodb.sameerdevops.online </app/schema/${component}.js &>>/tmp/Roboshop.log
       func_status_check $?
       elif [ "$schema_setup" == "mysql" ]; then
           func_print_head " install sql"
-          yum install mysql -y
+          yum install mysql -y &>>/tmp/Roboshop.log
           func_status_check $?
           func_print_head " load schema"
           #mysql -h mysql.sameerdevops.online -uroot -pRoboShop@1 < /app/schema/${component}.sql
-          mysql -h mysql.sameerdevops.online -uroot -p${mysql_root_pwd} < /app/schema/${component}.sql
+          mysql -h mysql.sameerdevops.online -uroot -p${mysql_root_pwd} < /app/schema/${component}.sql &>>/tmp/Roboshop.log
           func_status_check $?
 	fi
 }
@@ -77,13 +77,13 @@ func_schema_setup()
 func_nodejs()
 {
 	func_print_head download modeJ setup
-	curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+	curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/Roboshop.log
 	func_print_head Install nodejs
-	yum install nodejs -y
+	yum install nodejs -y &>>/tmp/Roboshop.log
 	func_status_check $?
 	func_app_prereq
 	func_print_head "install npm"
-	npm install
+	npm install &>/tmp/Roboshop.log
 	func_status_check $?
 	func_schema_setup
 	func_status_check $?
@@ -96,18 +96,18 @@ func_nodejs()
 func_java()
 {
   func_print_head " install java maven dependency"
-  yum install maven -y
+  yum install maven -y &>>/tmp/Roboshop.log
   func_status_check $?
   func_app_prereq
   func_print_head " Lets download the dependencies & build the application"
   cd /app
-  mvn clean package
-  mv target/${component}-1.0.jar ${component}.jar
+  mvn clean package &>/tmp/Roboshop.log
+  mv target/${component}-1.0.jar ${component}.jar &>>/tmp/Roboshop.log
   func_systemd_setup
   func_status_check $?
   func_schema_setup
   func_status_check $?
   func_print_head " restart the ${component} service"
-  systemctl restart ${component}
+  systemctl restart ${component} &>>/tmp/Roboshop.log
 }
 
