5698308fbcd17e5181ea8ad94e2e92f798e4d24f
sameeryalla <sameeryalla@gmail.com>
2023-05-05 23:19:35 +0530

    art file deleted

--- Cart.sh
+++
@@ -1,9 +0,0 @@
-REALPATH=$(realpath "$0")
-script_path=$(dirname "$REALPATH")
-source ${script_path}/common.sh
-component=cart
-
-func_print_head " setting up cart module through function using component variable"
-#function declared in common.sh
-func_nodejs
-func_print_head " end of cart module installation "
